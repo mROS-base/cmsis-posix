0349de54edd4fdc02ae7139e18ec1c9ef19ce0e1
Takashi Mori <tmori@esm.co.jp>
2021-11-19 09:33:26 +0900

    add build.bash

---
+++ build.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ $# -ne 0 -a $# -ne 1 ]
+then
+	echo "Usage: $0 {all|test|clean}"
+	exit 1
+fi
+
+OPT=${1}
+if [ $# -eq 0 ]
+then
+	OPT="all"
+fi
+
+echo $OPT
+
+cd cmake-build
+if [ ${OPT} = "clean" ]
+then
+	rm -rf ./*
+	exit 0
+fi
+
+cmake -D debug=true -D gcov=true ..
+make
+
+if [ ${OPT} = "test" ]
+then
+	make test
+	cd src/CMakeFiles/cmsis.dir/api
+	lcov -c -d . -o lcov.info
+	genhtml lcov.info -o ./info
+	cd ../core
+	lcov -c -d . -o lcov.info
+	genhtml lcov.info -o ./info
+fi
+
