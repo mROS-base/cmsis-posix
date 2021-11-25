694589026730a4d88fba38dc71184c6a8a8a472b
Takashi Mori <tmori@esm.co.jp>
2021-11-25 12:42:59 +0900

    add download.bash for application build

--- build.bash
+++ build.bash
@@ -1,30 +1,12 @@
 #!/bin/bash
 
-CMSIS_OS_BRANCH="mros2"
-CMSIS_OS_HDIR="Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
-CMSIS_OS_REPO=https://raw.githubusercontent.com/mROS-base/STM32CubeF7/${CMSIS_OS_BRANCH}/Middlewares/${CMSIS_OS_HDIR}
-
-function download_cmsis_osheader()
-{
-	if [ -d ${CMSIS_OS_HDIR} ]
-	then
-		:
-	else
-		wget "${CMSIS_OS_REPO}/cmsis_os.h"
-		wget "${CMSIS_OS_REPO}/cmsis_os2.h"
-		mkdir -p ${CMSIS_OS_HDIR}
-		mv cmsis_os.h ${CMSIS_OS_HDIR}/
-		mv cmsis_os2.h ${CMSIS_OS_HDIR}/
-	fi
-}
-
 if [ $# -ne 0 -a $# -ne 1 ]
 then
 	echo "Usage: $0 {all|test|clean}"
 	exit 1
 fi
 
-download_cmsis_osheader
+bash Third_Party/download.bash
 
 OPT=${1}
 if [ $# -eq 0 ]
