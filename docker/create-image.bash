b8ac10ff68a84fb89be9ac506cde1cdfd069f80a
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-10-16 10:31:49 +0900

    remove id_rsa file to create Docker image

--- docker/create-image.bash
+++ docker/create-image.bash
@@ -3,9 +3,9 @@
 DOCKER_IMAGE=`cat docker_image.txt`
 
 #TODO REMOVE HERE
-cp /home/tmori/.ssh/id_rsa .
+#cp /home/${USER}/.ssh/id_rsa .
 
 docker build -t ${DOCKER_IMAGE} -f Dockerfile .
 
 #TODO REMOVE HERE
-rm -f id_rsa
+#rm -f id_rsa
